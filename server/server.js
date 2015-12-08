5d3557d68049c2f8f497e01817f417331a666838
ccheung0926 <cindycheung0926@gmail.com>
2015-12-07 23:47:26 -0800

    basic setup

--- server/server.js
+++ server/server.js
@@ -1,4 +1,11 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + "/../cli"))
+app.use(express.static(__dirname + "/../client"));
+
+var server = app.listen(3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("listening to localhost", host, port);
+});
